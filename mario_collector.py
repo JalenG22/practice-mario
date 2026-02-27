322c478ab566c5f8ad6217d7a181ba42650c7a7d
Jalen Gilmore <scottjalen@me.com>
2026-02-27 04:38:47 -0500

    Update mario_collector.py
    Added Collector script

--- mario_collector.py
+++ mario_collector.py
@@ -1,1 +1,27 @@
+#Mario Collector
+#ERROR lines (coins), WARNING lines (bananas), and total lines
+
+#Variables
+coins = 0
+bananas = 0
+total_lines = 0
+
+#File
+with open("mario.kart", "r") as file: 
+
+#Loop 
+for line in file: 
+  total_lines += 1
+  
+  if "ERROR" in line:
+    coins += 1
+    
+  if "WARNING" in line: 
+    bananas += 1
+
+#Print Results
+print("Total lines:", total_lines)
+print("Coins collected:", coins)
+print("Bananas collected:", bananas)
+    
 
